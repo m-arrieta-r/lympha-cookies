fd8540c07a170151727cd002c9ac70a1f9f538f0
M.adrian15@gmail.com <Rush930@>
2016-07-09 11:31:26 -0600

    fixing minor issues

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
 
 var count = 0;
+var cookiesLength = 0;
 function toggle(tab) {
   var domainStr = extractDomain(tab.url);
+  console.log("domainStr "+domainStr);
   browser.cookies.getAll({domain: domainStr}, removeCookies);
 }
 
 function notify(count) {
   //console.log("background script received message");
-  var title = chrome.i18n.getMessage("notificationTitle");
-  var content = chrome.i18n.getMessage("notificationContent", count);
+  var title = browser.i18n.getMessage("notificationTitle");
+  var content = browser.i18n.getMessage("notificationContent", count);
   browser.notifications.create({
     "type": "basic",
     "iconUrl": browser.extension.getURL("data/icon.png"),
@@ -21,32 +23,37 @@ function logError(e) {
   console.error("Error. "+e);
 }
 
-function removeCookies(cookies) {
-  count = 0;
-  if(cookies.length === 0) {
-    notify("0");
+function notifyCokiesRemoved(cookie) {
+  count = (c !== null)?count+1:count;
+  if(cookiesLength === count) {
+    notify(count.toString());
   }
-  cookies.forEach((cookie, index, array) => {
-    var url = (cookie.secure)?"https://"+cookie.domain:"https://"+cookie.domain;
-    var removeCookie = browser.cookies.remove({ url: url, name: cookie.name, storeId: cookie.storeId});
-    removeCookie.then((c)=> {
-      count = (c !== null)?count+1:count;
-      if(cookies.length === count) {
-        notify(count.toString());
-      }
-    }, logError);
-  });
+}
 
+function removeCookie(cookie, index, array) {
+  count = 0;
+  var url = (cookie.secure)?"https://"+cookie.domain:"https://"+cookie.domain;
+  var removeCookie = browser.cookies.remove({ url: url, name: cookie.name, storeId: cookie.storeId});
+  removeCookie.then(notifyCokiesRemoved, logError);
+}
+
+function removeCookies(cookies) {
+  cookiesLength = cookies.length;
+  cookies.forEach(removeCookie);
+  if(cookiesLength == 0){
+    notify(cookiesLength.toString());
+  }
+}
+
+function hasProtocol(url) {
+  return (url.indexOf("://") > -1);
 }
 
 function extractDomain(url) {
     var domain;
+    var urlSplitted = url.split('/');
     //find & remove protocol (http, ftp, etc.) and get domain
-    if (url.indexOf("://") > -1) {
-        domain = url.split('/')[2];
-    } else {
-        domain = url.split('/')[0];
-    }
+    domain =  (hasProtocol(url)) ? urlSplitted[2] : urlSplitted[0];
     //find & remove port number
     domain = domain.split(':')[0];
     return domain.replace("www.", "");
@@ -72,7 +79,7 @@ function init() {
     title: chrome.i18n.getMessage("lymphaCtxBtn"),
     contexts: ["all"]
   });
-  chrome.contextMenus.onClicked.addListener(contextMenusClicked);
+  browser.contextMenus.onClicked.addListener(contextMenusClicked);
   browser.browserAction.onClicked.addListener(toggle);
   browser.commands.onCommand.addListener(commandsClicked);
 }
