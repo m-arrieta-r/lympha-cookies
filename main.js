5d926e1f8e18249426cdabdfe1edf636e5dd983c
Minor Arrieta <minor@grupoardigital.com>
2017-10-01 19:57:55 -0600

    Adding notifications parameter on options

--- main.js
+++ main.js
@@ -1,90 +1,115 @@
-
-var count = 0;
-var cookiesLength = 0;
+let options;
+let count = 0;
 
 function toggle(tab) {
-  const domainStr = extractDomain(tab.url);
-  console.log("domainStr " + domainStr);
-  browser.cookies.getAll({domain: domainStr}, removeCookies);
+    const domainStr = extractDomain(tab.url);
+
+    console.log("domainStr " + domainStr);
+    browser.cookies.getAll({
+        domain: domainStr
+    }, removeCookies);
 }
 
 function notify(count) {
-  //console.log("background script received message");
-  const title = browser.i18n.getMessage("notificationTitle");
-  const content = browser.i18n.getMessage("notificationContent", count);
-  browser.notifications.create({
-    "type": "basic",
-    "iconUrl": browser.extension.getURL("data/icon.png"),
-    "title": title,
-    "message": content,
-    "eventTime": 1000
-  });
+    if (options.lympha.notifications) {
+        const title = browser.i18n.getMessage("notificationTitle");
+        const content = browser.i18n.getMessage("notificationContent", count.toString());
+
+        browser.notifications.create('lympha-notification', {
+            "type": "basic",
+            "iconUrl": browser.extension.getURL("data/icon.png"),
+            "title": title,
+            "message": content
+        });
+    }
+
 }
 
 function logError(e) {
-  console.error("Error. " + e);
-}
-
-function notifyCokiesRemoved(cookie) {
-  count = (c) ? count + 1 : count;
-  if (cookiesLength === count) {
-    notify(count.toString());
-  }
+    console.error("Error. " + e);
 }
 
 function removeCookie(cookie, index, array) {
-  count = 0;
-  const protocol = (cookie.secure) ? "https://" : "http://";
-  const url = protocol + cookie.domain
-  const removeCookie = browser.cookies.remove({ url: url, name: cookie.name, storeId: cookie.storeId});
-  removeCookie.then(notifyCokiesRemoved, logError);
+    const protocol = (cookie.secure) ? "https://" : "http://";
+    const url = protocol + cookie.domain
+    const removeCookie = browser.cookies.remove({
+        url: url,
+        name: cookie.name,
+        storeId: cookie.storeId
+    });
+
+    removeCookie.then((cookie) => {
+        const isTheLastOne = array.length === index + 1;
+        if (cookie) {
+            count++;
+        }
+        if (isTheLastOne) {
+            notify(count.toString());
+        }
+    }, logError);
 }
 
 function removeCookies(cookies) {
-  cookiesLength = cookies.length;
-  cookies.forEach(removeCookie);
-  if (cookiesLength === 0){
-    notify(cookiesLength.toString());
-  }
+    count = 0;
+
+    if (cookies.length === 0) {
+        notify(cookies.length);
+    } else {
+        cookies.forEach(removeCookie);
+    }
 }
 
 function hasProtocol(url) {
-  return (url.indexOf("://") > -1);
+    return (url.indexOf("://") > -1);
 }
 
 function extractDomain(url) {
     let domain;
     const urlSplitted = url.split('/');
     //find & remove protocol (http, ftp, etc.) and get domain
-    domain =  (hasProtocol(url)) ? urlSplitted[2] : urlSplitted[0];
+    domain = (hasProtocol(url)) ? urlSplitted[2] : urlSplitted[0];
     //find & remove port number
     domain = domain.split(':')[0];
     return domain.replace("www.", "");
 }
 
 function contextMenusClicked(info, tab) {
- if (info.menuItemId === "lympha-ctx-btn") {
-   toggle(tab);
- }
+    if (info.menuItemId === "lympha-ctx-btn") {
+        toggle(tab);
+    }
 }
 
 function commandsClicked(command) {
-  if (command === "toggle-feature") {
-    browser.tabs.query({active: true}, (tabArray) => {
-      toggle(tabArray[0]);
+    if (command === "toggle-feature") {
+        browser.tabs.query({
+            active: true
+        }, (tabArray) => {
+            toggle(tabArray[0]);
+        });
+    }
+}
+
+function loadOptions() {
+    let gettingOptions = browser.storage.sync.get({
+        "lympha": {
+            "notifications": true
+        }
     });
-  }
+    gettingOptions.then((obj) => {
+        options = obj;
+    });
 }
 
 function init() {
-  browser.contextMenus.create({
-    id: "lympha-ctx-btn",
-    title: chrome.i18n.getMessage("lymphaCtxBtn"),
-    contexts: ["all"]
-  });
-  browser.contextMenus.onClicked.addListener(contextMenusClicked);
-  browser.browserAction.onClicked.addListener(toggle);
-  browser.commands.onCommand.addListener(commandsClicked);
+    loadOptions();
+    browser.contextMenus.create({
+        id: "lympha-ctx-btn",
+        title: chrome.i18n.getMessage("lymphaCtxBtn"),
+        contexts: ["all"]
+    });
+    browser.contextMenus.onClicked.addListener(contextMenusClicked);
+    browser.browserAction.onClicked.addListener(toggle);
+    browser.commands.onCommand.addListener(commandsClicked);
 }
 
 init();
---
+++ options/options.js
@@ -0,0 +1,25 @@
+function save(event) {
+    event.preventDefault();
+    const notifications = document.getElementById('notifications');
+    const options = browser.storage.sync.set({
+        "lympha": {
+            "notifications": notifications.checked
+        }
+    });
+}
+
+function restore() {
+    const notifications = document.getElementById('notifications');
+    const options = browser.storage.sync.get({
+        "lympha": {
+            "notifications": true
+        }
+    });
+    options.then((res) => {
+        notifications.checked = res.lympha.notifications
+    });
+}
+
+
+document.addEventListener('DOMContentLoaded', restore);
+document.querySelector("form").addEventListener("submit", save);
